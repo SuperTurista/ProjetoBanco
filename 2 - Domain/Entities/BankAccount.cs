cde962207182ab249df8c1d2101beeac0b0c8d0b
Gabriel Nascimento <binho-nascimento@hotmail.com>
2021-07-24 03:23:41 -0300

    funcionalidade de deposito e saque bancário

--- 2 - Domain/Entities/BankAccount.cs
+++ 2 - Domain/Entities/BankAccount.cs
@@ -8,22 +8,28 @@ namespace Domain.Entities
         {
             UserId = userId;
         }
-        
-        public Guid UserId { get; set; }      
-        
+
+        public Guid UserId { get; set; }
+
         public long Number { get; set; }
 
         public string Agency { get; set; }
 
         public string Type { get; set; }
 
-        public decimal Balance { get; set; }
-        
-        
-        
-        
-        
-        
+        public decimal Balance { get; private set; }
+
+        public void BankDeposit(decimal deposit)
+        {
+            if (deposit > 0)
+                Balance += deposit;
+        }
+
+        public void BankDraft(decimal draft) 
+        { 
+            if(Balance > 0 && draft > 0 && Balance >= draft)
+                Balance -= draft;
+        }
         
         
     }
