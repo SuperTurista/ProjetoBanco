11286d0bcbaf67f949c8a9f734720ee72f878d7a
Gabriel Nascimento <binho-nascimento@hotmail.com>
2021-07-24 01:10:57 -0300

    Relação entre conta bancaria e usuário

--- 2 - Domain/Entities/BankAccount.cs
+++ 2 - Domain/Entities/BankAccount.cs
@@ -1,4 +1,30 @@
-namespace Domain
-{
+using System;
 
+namespace Domain.Entities
+{
+    public class BankAccount : Entity
+    {
+        public BankAccount(Guid userId)
+        {
+            UserId = userId;
+        }
+        
+        public Guid UserId { get; set; }      
+        
+        public long Number { get; set; }
+
+        public string Agency { get; set; }
+
+        public string Type { get; set; }
+
+        public decimal Balance { get; set; }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
 }
